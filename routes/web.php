75580f0c959afd331cd3faad4caccdf1895d699c
SilverBlitzy <anabeatriz.lana@estudante.ufjf.br>
2022-08-27 21:36:20 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware(['auth', 'verified'])->group(function () {
+
+    // >>> insira suas rotas aqui !!!!! <<<
+    
+    Route::get('/', function () {
+        return view('dashboard');
+    })/*->middleware('auth')*/;
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })/*->middleware(['auth'])*/->name('dashboard');
+    
+});
+
+
+Route::get('/dale', function() {
+    return view('dale');
+});
+
+require __DIR__.'/auth.php';
